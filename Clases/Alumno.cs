2403017b45eb8f7894afa5f4ecd97c2a36d792ad
José López <jeduardo14lopez@gmail.com>
2023-09-05 09:35:05 -0600

    Add files via upload

---
+++ Clases/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD2023.Clases
+{
+    public class Alumno
+    {
+        public int Carne { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Celular { get; set; }
+        public int TelefonoCasa  { get; set; }
+    }
+    
+}
---
+++ Interfaces/IAlumno.cs
@@ -0,0 +1,31 @@
+﻿using CRUD2023.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD2023.Interfaces
+{
+    internal interface IAlumno : ITest
+    {
+        //Template-> Plantilla -> Machote -> Esquema
+        //NO tiene funcionalidad , solo declaracion de metodos.
+        //Camel Case
+
+        public int GetIdAlumno(Alumno alumno);
+        public int GetIdAlumno(String Nombre);
+        public Alumno GetAlumno(int id);
+        public List<Alumno> GetAllAlumnos();
+        public void SaveAlumno(Alumno alumno);
+        public void DeleteAlumno(int id);
+        public void UpdateAlumno(Alumno alumno, bool forze= false);
+
+    }
+
+    interface ITest
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
